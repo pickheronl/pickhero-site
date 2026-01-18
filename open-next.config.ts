13d85bd8edbfd4517328e97c38494d91b994180e
Len van Essen <len@wndr.digital>
2026-01-18 21:30:43 +0100

    Test deploy

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,17 +1,10 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare'
-import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache'
-import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache'
+import staticAssetsCache from '@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache'
 import doShardedTagCache from '@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache'
-import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue'
-import { purgeCache } from '@opennextjs/cloudflare/overrides/cache-purge/index'
 
 export default defineCloudflareConfig({
-  incrementalCache: withRegionalCache(r2IncrementalCache, { mode: 'long-lived' }),
-  queue: doQueue,
-  // This is only required if you use On-demand revalidation
+  // Static assets cache bundles with the worker, no R2 bulk upload needed
+  incrementalCache: staticAssetsCache,
   tagCache: doShardedTagCache({ baseShardSize: 12 }),
-  // Disable this if you want to use PPR
   enableCacheInterception: true,
-  // you can also use the `durableObject` option to use a durable object as a cache purge
-  cachePurge: purgeCache({ type: 'direct' }),
 })
