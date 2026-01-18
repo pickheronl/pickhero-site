c3b9f05518752af86cf8ff935804123396b76850
Len van Essen <len@wndr.digital>
2026-01-18 22:05:44 +0100

    Purge cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,11 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare'
 import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache'
 import doShardedTagCache from '@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache'
+import { purgeCache } from '@opennextjs/cloudflare/overrides/cache-purge/index'
 
 export default defineCloudflareConfig({
   incrementalCache: r2IncrementalCache,
   tagCache: doShardedTagCache({ baseShardSize: 12 }),
   enableCacheInterception: true,
+  cachePurge: purgeCache({ type: 'direct' }),
 })
