05c8449a41643320f3c618025c7e47d56dbd297e
Len van Essen <len@wndr.digital>
2026-01-18 20:32:29 +0100

    Caching

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,17 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from '@opennextjs/cloudflare/config'
+import { defineCloudflareConfig } from '@opennextjs/cloudflare'
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache'
+import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache'
+import doShardedTagCache from '@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache'
+import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue'
+import { purgeCache } from '@opennextjs/cloudflare/overrides/cache-purge/index'
 
-export default defineCloudflareConfig({})
+export default defineCloudflareConfig({
+  incrementalCache: withRegionalCache(r2IncrementalCache, { mode: 'long-lived' }),
+  queue: doQueue,
+  // This is only required if you use On-demand revalidation
+  tagCache: doShardedTagCache({ baseShardSize: 12 }),
+  // Disable this if you want to use PPR
+  enableCacheInterception: true,
+  // you can also use the `durableObject` option to use a durable object as a cache purge
+  cachePurge: purgeCache({ type: 'direct' }),
+})
