5d974611637811d2a1d49438cd208372a323709b
Len van Essen <len@wndr.digital>
2026-01-18 21:42:37 +0100

    Update

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,14 +1,10 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare'
-import doShardedTagCache from '@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache'
-import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache'
 import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache'
+import doShardedTagCache from '@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache'
 
 export default defineCloudflareConfig({
   // Static assets cache bundles with the worker, no R2 bulk upload needed
-  incrementalCache: withRegionalCache(r2IncrementalCache, {
-    mode: 'long-lived',
-    bypassTagCacheOnCacheHit: true,
-  }),
+  incrementalCache: r2IncrementalCache,
   tagCache: doShardedTagCache({ baseShardSize: 12 }),
   enableCacheInterception: true,
 })
